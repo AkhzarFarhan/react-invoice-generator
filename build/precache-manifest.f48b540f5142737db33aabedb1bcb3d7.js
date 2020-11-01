2d74d5431b44cc801070c48ed277f599d3f6b6a1
AkhzarFarhan <atakhzar@gmail.com>
2020-11-01 17:10:06 +0000

    build push

---
+++ build/precache-manifest.f48b540f5142737db33aabedb1bcb3d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcaac435b092c05f7ab55a8182a2586f",
+    "url": "/react-invoice-generator/index.html"
+  },
+  {
+    "revision": "2bb86ed27437c3f66eed",
+    "url": "/react-invoice-generator/static/css/main.bea38237.chunk.css"
+  },
+  {
+    "revision": "f52815fb71adc16bc034",
+    "url": "/react-invoice-generator/static/js/2.393f57bd.chunk.js"
+  },
+  {
+    "revision": "908d67519da53f5d237f0100de94c58c",
+    "url": "/react-invoice-generator/static/js/2.393f57bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bb86ed27437c3f66eed",
+    "url": "/react-invoice-generator/static/js/main.9e4fa471.chunk.js"
+  },
+  {
+    "revision": "aabb303fe90f96d72bbb",
+    "url": "/react-invoice-generator/static/js/runtime-main.33c79cdc.js"
+  },
+  {
+    "revision": "5af81432dda1dde2def604f8052f424a",
+    "url": "/react-invoice-generator/static/media/download.5af81432.svg"
+  }
+]);
